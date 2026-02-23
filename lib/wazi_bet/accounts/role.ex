dbdfdfc12123e9d590f44223c176a4688b937a47
Washington <jaywashie5735@gmail.com>
2026-02-23 12:11:22 +0300

    add role.ex

--- lib/wazi_bet/accounts/role.ex
+++ lib/wazi_bet/accounts/role.ex
@@ -6,18 +6,27 @@ defmodule WaziBet.Accounts.Role do
   alias WaziBet.Accounts.Permission
 
   schema "roles" do
-    field :name, :string
+    field :role, :string
+    field :slug, :string
 
 
-    many_to_many :users,User, join_through: "users_roles"
-    many_to_many :permissions, Permission, join_through: "roles_permissions"
+    many_to_many :users,User, join_through: "user_roles"
+    many_to_many :permissions, Permission, join_through: "role_permissions"
     timestamps()
   end
 
 
   def changeset(role, attrs) do
     role
-    |> cast(attrs, [:name])
-    |> validate_required([:name])
+    |> cast(attrs, [:role])
+    |> validate_required([:role])
+    |> generate_slug()
+  end
+
+  def generate_slug(changeset) do
+    case get_change(changeset, :role) do
+      nil -> changeset
+      role_str -> put_change(changeset, :slug, Slug.slugify(role_str))
+    end
   end
 end
