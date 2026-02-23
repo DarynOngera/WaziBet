b10f9d1835bf6c8fcea122e7d0ddf4c4d7863974
Washington <jaywashie5735@gmail.com>
2026-02-23 11:20:04 +0300

    Fix user.ex

--- lib/wazi_bet/accounts/user.ex
+++ lib/wazi_bet/accounts/user.ex
@@ -2,93 +2,85 @@ defmodule WaziBet.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias WaziBet.Bets.Betslip
+  alias WaziBet.Accounts.Role
+
+  @min_password_length 12
+  @max_password_length 72
+
   schema "users" do
     field :email, :string
-    field :password, :string, virtual: true, redact: true
+    field :first_name, :string
+    field :last_name, :string
+    field :msisdn, :string
+    field :balance, :decimal, default: Decimal.new("1000.0")
     field :hashed_password, :string, redact: true
+    field :deleted_at, :utc_datetime
     field :confirmed_at, :utc_datetime
-    field :authenticated_at, :utc_datetime, virtual: true
+    field :authenticated_at, :utc_datetime
 
+    # Virtual fields
+    field :password, :string, virtual: true, redact: true
+    field :password_confirmation, :string, virtual: true, redact: true
+
+    many_to_many :roles, Role, join_through: "user_roles"
     has_many :betslips, Betslip
-    many_to_many :roles, Role, join_through: "users_roles", on_replace: :delete
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
 
-  @doc """
-  A user changeset for registering or changing the email.
+ def registration_changeset(user, attrs, opts \\ []) do
+  user
+  |> cast(attrs, [
+    :email,
+    :password,
+    :password_confirmation,
+    :balance,
+    :first_name,
+    :last_name,
+    :msisdn
+  ])
+  |> validate_required([
+    :email,
+    :password,
+    :password_confirmation,
+    :first_name,
+    :last_name,
+    :msisdn
+  ])
+  |> validate_email(opts)
+  |> validate_password(opts)
+  |> validate_confirmation(:password, message: "does not match password")
+  |> validate_balance()
+  |> validate_length(:first_name, max: 100)
+  |> validate_length(:last_name, max: 100)
+  |> validate_format(:msisdn, ~r/^\+?[0-9]+$/, message: "must be a valid phone number")
+  |> unique_constraint(:email)
 
-  It requires the email to change otherwise an error is added.
-
-  ## Options
-
-    * `:validate_unique` - Set to false if you don't want to validate the
-      uniqueness of the email, useful when displaying live validations.
-      Defaults to `true`.
-  """
-  def email_changeset(user, attrs, opts \\ []) do
-    user
-    |> cast(attrs, [:email])
-    |> validate_email(opts)
-  end
+end
 
   defp validate_email(changeset, opts) do
-    changeset =
-      changeset
-      |> validate_required([:email])
-      |> validate_format(:email, ~r/^[^@,;\s]+@[^@,;\s]+$/,
-        message: "must have the @ sign and no spaces"
-      )
-      |> validate_length(:email, max: 160)
+    changeset
+    |> validate_required([:email])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> validate_length(:email, max: 160)
+    |> maybe_validate_unique_email(opts)
+  end
 
-    if Keyword.get(opts, :validate_unique, true) do
+  defp maybe_validate_unique_email(changeset, opts) do
+    if Keyword.get(opts, :validate_email, true) do
       changeset
       |> unsafe_validate_unique(:email, WaziBet.Repo)
       |> unique_constraint(:email)
-      |> validate_email_changed()
     else
       changeset
     end
-  end
-
-  defp validate_email_changed(changeset) do
-    if get_field(changeset, :email) && get_change(changeset, :email) == nil do
-      add_error(changeset, :email, "did not change")
-    else
-      changeset
-    end
-  end
-
-  @doc """
-  A user changeset for changing the password.
-
-  It is important to validate the length of the password, as long passwords may
-  be very expensive to hash for certain algorithms.
-
-  ## Options
-
-    * `:hash_password` - Hashes the password so it can be stored securely
-      in the database and ensures the password field is cleared to prevent
-      leaks in the logs. If password hashing is not needed and clearing the
-      password field is not desired (like when using this changeset for
-      validations on a LiveView form), this option can be set to `false`.
-      Defaults to `true`.
-  """
-  def password_changeset(user, attrs, opts \\ []) do
-    user
-    |> cast(attrs, [:password])
-    |> validate_confirmation(:password, message: "does not match password")
-    |> validate_password(opts)
   end
 
   defp validate_password(changeset, opts) do
     changeset
     |> validate_required([:password])
-    |> validate_length(:password, min: 12, max: 72)
-    # Examples of additional password validation:
-    # |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
-    # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
-    # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
+    |> validate_length(:password, min: @min_password_length, max: @max_password_length)
     |> maybe_hash_password(opts)
   end
 
@@ -98,9 +90,8 @@ defmodule WaziBet.Accounts.User do
 
     if hash_password? && password && changeset.valid? do
       changeset
-      # Hashing could be done with `Ecto.Changeset.prepare_changes/2`, but that
-      # would keep the database transaction open longer and hurt performance.
-      |> put_change(:hashed_password, Pbkdf2.hash_pwd_salt(password))
+      |> validate_length(:password, max: @max_password_length, count: :bytes)
+      |> put_change(:hashed_password, Bcrypt.hash_pwd_salt(password))
       |> delete_change(:password)
     else
       changeset
@@ -108,26 +99,63 @@ defmodule WaziBet.Accounts.User do
   end
 
   @doc """
-  Confirms the account by setting `confirmed_at`.
+  A user changeset for changing the email.
+
+  It requires the email to be validated and checks for uniqueness.
   """
+  def email_changeset(user, attrs, opts \\ []) do
+    user
+    |> cast(attrs, [:email])
+    |> validate_email(opts)
+    |> case do
+      %{changes: %{email: _}} = changeset -> changeset
+      %{} = changeset -> add_error(changeset, :email, "did not change")
+    end
+  end
+
+  def password_changeset(user, attrs, opts \\ []) do
+    user
+    |> cast(attrs, [:password])
+    |> validate_confirmation(:password, message: "does not match password")
+    |> validate_password(opts)
+  end
+
   def confirm_changeset(user) do
-    now = DateTime.utc_now(:second)
+    now = DateTime.utc_now() |> DateTime.truncate(:second)
     change(user, confirmed_at: now)
   end
 
-  @doc """
-  Verifies the password.
-
-  If there is no user or the user doesn't have a password, we call
-  `Pbkdf2.no_user_verify/0` to avoid timing attacks.
-  """
   def valid_password?(%WaziBet.Accounts.User{hashed_password: hashed_password}, password)
       when is_binary(hashed_password) and byte_size(password) > 0 do
-    Pbkdf2.verify_pass(password, hashed_password)
+    Bcrypt.verify_pass(password, hashed_password)
   end
 
   def valid_password?(_, _) do
-    Pbkdf2.no_user_verify()
+    Bcrypt.no_user_verify()
     false
   end
+
+  def validate_current_password(changeset, password) do
+    if valid_password?(changeset.data, password) do
+      changeset
+    else
+      add_error(changeset, :current_password, "is not valid")
+    end
+  end
+
+  def balance_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:balance])
+    |> validate_required([:balance])
+    |> validate_balance()
+  end
+
+  defp validate_balance(changeset) do
+    changeset
+    |> validate_number(:balance, greater_than_or_equal_to: 0)
+    |> check_constraint(:balance, name: :balance_must_be_non_negative)
+  end
+
+  def confirmed?(%WaziBet.Accounts.User{confirmed_at: nil}), do: false
+  def confirmed?(%WaziBet.Accounts.User{}), do: true
 end
