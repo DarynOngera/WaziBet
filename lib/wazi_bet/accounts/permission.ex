cef7e8a6085b3c204a1c010a613e4c4245d22baf
Washington <jaywashie5735@gmail.com>
2026-02-23 14:41:43 +0300

    add permission.ex

--- lib/wazi_bet/accounts/permission.ex
+++ lib/wazi_bet/accounts/permission.ex
@@ -4,19 +4,27 @@ defmodule WaziBet.Accounts.Permission do
   alias WaziBet.Accounts.Role
 
   schema "permissions" do
-    field :name, :string
-    field :description, :string
+    field :permission, :string
+    field :slug, :string
 
-    many_to_many :roles, Role, join_through: "roles_permissions"
+    many_to_many :roles, Role, join_through: "role_permissions"
 
     timestamps(type: :utc_datetime)
   end
 
-  
+
   def changeset(permission, attrs) do
     permission
-    |> cast(attrs, [:name, :description])
-    |> validate_required([:name, :description])
-    |> unique_constraint(:name)
+    |> cast(attrs, [:permission])
+    |> validate_required([:permission])
+    |> unique_constraint(:permission)
+    |> generate_slug()
+  end
+
+  def generate_slug(changeset) do
+    case get_change(changeset, :permission) do
+      nil -> changeset
+      permission_str -> put_change(changeset, :slug, Slug.slugify(permission_str))
+    end
   end
 end
