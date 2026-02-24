ed773c297cc2f14d8bd9ca3b701a151ecd07a72c
Ongera <ongeradaryn@gmail.com>
2026-02-24 10:58:35 +0300

    chore: rewrite bet scope

---
+++ lib/wazi_bet/bets/pending_betslip.ex
@@ -0,0 +1,20 @@
+defmodule WaziBet.Bets.PendingBetslip do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "pending_betslips" do
+    field :stake, :decimal
+    field :selections, {:array, :map}
+    field :user_id, :integer
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(pending_betslip, attrs) do
+    pending_betslip
+    |> cast(attrs, [:user_id, :stake, :selections])
+    |> validate_required([:user_id, :stake, :selections])
+    |> validate_number(:stake, greater_than: 0)
+  end
+end
