c6ef1d0011f1063b35204ce6a5720bf41d37ed04
Ongera <ongeradaryn@gmail.com>
2026-03-02 11:20:12 +0300

    test: mailing test

---
+++ lib/wazi_bet/mail/demo.ex
@@ -0,0 +1,12 @@
+defmodule WaziBet.Sample.UserEmail do
+  import Swoosh.Email
+
+  def welcome(user) do
+    new()
+    |> to({user.name, user.email})
+    |> from({"Ongera", "ongeradaryn@gmail.com"})
+    |> subject("Hello, Avengers!")
+    |> html_body("<h1>Hello #{user.name}</h1>")
+    |> text_body("Hello #{user.name}\n")
+  end
+end
