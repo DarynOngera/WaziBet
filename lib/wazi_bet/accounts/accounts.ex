f4dd544b846c6635d0bf3368a20b6fd05e20973f
Ongera <ongeradaryn@gmail.com>
2026-03-03 09:24:15 +0300

    accounts

--- lib/wazi_bet/accounts/accounts.ex
+++ lib/wazi_bet/accounts/accounts.ex
@@ -1,26 +1,159 @@
 defmodule WaziBet.Accounts do
   @moduledoc """
-  User management and balance operations.
+  The Accounts context.
+
+  This module handles:
+  - User management (registration, authentication, settings)
+  - Balance operations
+  - Role and permission management
   """
 
-  import Ecto.Query
-
-  alias WaziBet.Accounts.User
+  import Ecto.Query, warn: false
   alias WaziBet.Repo
+  alias WaziBet.Accounts.{User, UserToken, UserNotifier, Role, Permission}
 
-  def create_user(attrs) do
+  ## Database getters
+
+  def get_user_by_email(email) when is_binary(email) do
+    Repo.get_by(User, email: email)
+  end
+
+  def get_user_by_email_and_password(email, password)
+      when is_binary(email) and is_binary(password) do
+    user = Repo.get_by(User, email: email)
+    if User.valid_password?(user, password), do: user
+  end
+
+  def get_user!(id), do: Repo.get!(User, id)
+
+  def get_user_with_roles!(id) do
+    User
+    |> Repo.get!(id)
+    |> Repo.preload(roles: [:permissions])
+  end
+
+  ## User registration
+
+  def register_user(attrs) do
     %User{}
     |> User.registration_changeset(attrs)
     |> Repo.insert()
+    |> case do
+      {:ok, user} ->
+        case Repo.get_by(Role, slug: "user") do
+          nil ->
+            {:ok, user}
+
+          role ->
+            assign_role_to_user(user.id, role.id)
+            {:ok, user}
+        end
+
+      error ->
+        error
+    end
   end
 
-  def get_user!(id) do
-    Repo.get!(User, id)
+  def create_user(attrs) do
+    register_user(attrs)
   end
 
-  def get_user_by_email(email) do
-    Repo.get_by(User, email: email)
+  def change_user_email(user, attrs \\ %{}, opts \\ []) do
+    User.email_changeset(user, attrs, opts)
   end
+
+  def update_user_email(user, token) do
+    context = "change:#{user.email}"
+
+    Repo.transact(fn ->
+      with {:ok, query} <- UserToken.verify_change_email_token_query(token, context),
+           %UserToken{sent_to: email} <- Repo.one(query),
+           {:ok, user} <- Repo.update(User.email_changeset(user, %{email: email})),
+           {_count, _result} <-
+             Repo.delete_all(from(UserToken, where: [user_id: ^user.id, context: ^context])) do
+        {:ok, user}
+      else
+        _ -> {:error, :transaction_aborted}
+      end
+    end)
+  end
+
+  def change_user_password(user, attrs \\ %{}, opts \\ []) do
+    User.password_changeset(user, attrs, opts)
+  end
+
+  def update_user_password(user, attrs) do
+    user
+    |> User.password_changeset(attrs)
+    |> update_user_and_delete_all_tokens()
+  end
+
+  ## Session
+
+  def generate_user_session_token(user) do
+    {token, user_token} = UserToken.build_session_token(user)
+    Repo.insert!(user_token)
+    token
+  end
+
+  def get_user_by_session_token(token) do
+    {:ok, query} = UserToken.verify_session_token_query(token)
+    Repo.one(query)
+  end
+
+  def get_user_by_magic_link_token(token) do
+    with {:ok, query} <- UserToken.verify_magic_link_token_query(token),
+         {user, _token} <- Repo.one(query) do
+      user
+    else
+      _ -> nil
+    end
+  end
+
+  def login_user_by_magic_link(token) do
+    {:ok, query} = UserToken.verify_magic_link_token_query(token)
+
+    case Repo.one(query) do
+      {%User{confirmed_at: nil, hashed_password: hash}, _token} when not is_nil(hash) ->
+        raise """
+        magic link log in is not allowed for unconfirmed users with a password set!
+        """
+
+      {%User{confirmed_at: nil} = user, _token} ->
+        user
+        |> User.confirm_changeset()
+        |> update_user_and_delete_all_tokens()
+
+      {user, token} ->
+        Repo.delete!(token)
+        {:ok, {user, []}}
+
+      nil ->
+        {:error, :not_found}
+    end
+  end
+
+  def deliver_user_update_email_instructions(%User{} = user, current_email, update_email_url_fun)
+      when is_function(update_email_url_fun, 1) do
+    {encoded_token, user_token} = UserToken.build_email_token(user, "change:#{current_email}")
+
+    Repo.insert!(user_token)
+    UserNotifier.deliver_update_email_instructions(user, update_email_url_fun.(encoded_token))
+  end
+
+  def deliver_login_instructions(%User{} = user, magic_link_url_fun)
+      when is_function(magic_link_url_fun, 1) do
+    {encoded_token, user_token} = UserToken.build_email_token(user, "login")
+    Repo.insert!(user_token)
+    UserNotifier.deliver_login_instructions(user, magic_link_url_fun.(encoded_token))
+  end
+
+  def delete_user_session_token(token) do
+    Repo.delete_all(from(UserToken, where: [token: ^token, context: "session"]))
+    :ok
+  end
+
+  ## Balance Operations
 
   def update_balance(user, amount) do
     user
@@ -43,8 +176,144 @@ defmodule WaziBet.Accounts do
     update_balance(user, new_balance)
   end
 
+  def update_user_balance(user, new_balance) do
+    update_balance(user, new_balance)
+  end
+
   def lock_user_for_update(user_id) do
     from(u in User, where: u.id == ^user_id, lock: "FOR UPDATE")
     |> Repo.one()
   end
+
+
+  def create_role(role, attrs) do
+    role
+    |> Role.changeset(attrs)
+    |> Repo.insert()
+  end 
+
+  ## Permission Checking
+
+  def user_has_permission?(user_id, permission_slug) when is_binary(permission_slug) do
+    permission_slug in get_user_permission_slugs(user_id)
+  end
+
+  def get_user_permission_slugs(user_id) do
+    Repo.all(
+      from p in Permission,
+        join: rp in "role_permissions",
+        on: rp.permission_id == p.id,
+        join: r in Role,
+        on: r.id == rp.role_id,
+        join: ur in "user_roles",
+        on: ur.role_id == r.id,
+        where: ur.user_id == ^user_id,
+        select: p.slug,
+        distinct: true
+    )
+  end
+
+  def get_user_with_permissions(user_id) do
+    User
+    |> Repo.get(user_id)
+    |> Repo.preload(roles: [:permissions])
+  end
+
+  def assign_role_to_user(user_id, role_id) do
+    Repo.insert_all("user_roles", [
+      %{
+        user_id: user_id,
+        role_id: role_id,
+        inserted_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
+        updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+      }
+    ])
+  end
+
+  def remove_role_from_user(user_id, role_id) do
+    from(ur in "user_roles",
+      where: ur.user_id == ^user_id and ur.role_id == ^role_id
+    )
+    |> Repo.delete_all()
+  end
+
+  def list_roles_with_permissions do
+    Role
+    |> Repo.all()
+    |> Repo.preload(:permissions)
+  end
+
+  def list_users(opts \\ []) do
+    page = Keyword.get(opts, :page, 1)
+    page_size = Keyword.get(opts, :page_size, 10)
+
+    User
+    |> where([u], is_nil(u.deleted_at))
+    |> order_by([u], u.id)
+    |> limit(^page_size)
+    |> offset(^((page - 1) * page_size))
+    |> Repo.all()
+    |> Repo.preload(:roles)
+  end
+
+  def count_users do
+    User
+    |> where([u], is_nil(u.deleted_at))
+    |> Repo.aggregate(:count, :id)
+  end
+
+  def get_user_with_betslips!(id) do
+    User
+    |> Repo.get!(id)
+    |> Repo.preload(betslips: [:selections], roles: [:permissions])
+  end
+
+  def soft_delete_user(user) do
+    user
+    |> User.soft_delete_changeset()
+    |> Repo.update()
+  end
+
+  def list_roles do
+    Repo.all(Role)
+  end
+
+  def get_role!(id), do: Repo.get!(Role, id)
+
+  def user_has_role?(user_id, role_slug) do
+    from(r in Role,
+      join: ur in "user_roles",
+      on: ur.role_id == r.id,
+      where: ur.user_id == ^user_id and r.slug == ^role_slug,
+      select: count(r.id)
+    )
+    |> Repo.one() > 0
+  end
+
+  ## Token helper
+
+  defp update_user_and_delete_all_tokens(changeset) do
+    Repo.transact(fn ->
+      with {:ok, user} <- Repo.update(changeset) do
+        tokens_to_expire = Repo.all(from(t in UserToken, where: t.user_id == ^user.id))
+
+        Repo.delete_all(from(t in UserToken, where: t.id in ^Enum.map(tokens_to_expire, & &1.id)))
+
+        {:ok, {user, tokens_to_expire}}
+      end
+    end)
+  end
+
+  ## Settings
+
+  @doc """
+  Checks whether the user is in sudo mode.
+  """
+  def sudo_mode?(user, minutes \\ -20)
+
+  def sudo_mode?(%User{authenticated_at: ts}, minutes) when is_struct(ts, DateTime) do
+    DateTime.after?(ts, DateTime.utc_now() |> DateTime.add(minutes, :minute))
+  end
+
+  def sudo_mode?(_user, _minutes), do: false
 end
