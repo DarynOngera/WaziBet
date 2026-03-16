a484e42b73d3d974f01ca680045ef5de997675fb
Ongera <ongeradaryn@gmail.com>
2026-03-16 12:13:32 +0300

    remove debug logging

--- lib/wazi_bet/workers/game_start_worker.ex
+++ lib/wazi_bet/workers/game_start_worker.ex
@@ -19,29 +19,22 @@ defmodule WaziBet.Workers.GameStartWorker do
 
   @impl true
   def perform(%Oban.Job{args: %{"game_id" => game_id}}) do
-    IO.puts("GameStartWorker: Starting game #{game_id}")
 
     try do
       game = Sport.get_game_with_teams!(game_id)
-      IO.puts("GameStartWorker: Game status is #{game.status}")
 
       # Only start if still scheduled
       if game.status == :scheduled do
-        IO.puts("GameStartWorker: Transitioning game to live")
-
         # Transition to live
         {:ok, _} = Sport.transition_game_status(game, :live)
-        IO.puts("GameStartWorker: Game transitioned to live")
 
         Phoenix.PubSub.broadcast( WaziBet.PubSub, "games", {__MODULE__, game_id, :started})
 
         # Close betting on all outcomes
         Bets.close_outcomes_for_game(game_id)
-        IO.puts("GameStartWorker: Outcomes closed")
 
         # Start the game simulation
         GameSupervisor.start_game(game)
-        IO.puts("GameStartWorker: Game simulation started")
       else
         IO.puts("GameStartWorker: Game not started - status is #{game.status}")
       end
